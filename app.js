286c5cda3c81db2d55985ce4955b805faffc5d1f
Huy Huỳnh Nguyễn Minh <dev.hnmhuy@gmail.com>
2023-11-14 21:41:33 +0700

    Add test map and instruction

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.use("/public", express.static(path.join(__dirname, "src", "public")));
+
 app.get("/", function (req, res) {
-    res.sendFile("Hello world");
+    res.sendFile(__dirname + "/testmap.html");
 });
 
 app.listen(3000);
