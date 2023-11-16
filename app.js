23d02f364700816bd2b094d9c93e1de62c892b68
NhatPhi1804 <kenshiro.pn@gmail.com>
2023-11-17 02:07:03 +0700

    change static syntax

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require("path")
 const port = 3000 | process.env.port
 const expressHbs = require("express-handlebars")
-app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(express.static(__dirname + "/public"));
 
 
 app.engine("hbs", expressHbs.engine({
@@ -24,9 +25,6 @@ app.use("/home", require('./routes/district/home.route'))
 app.use("/location", require('./routes/district/location.route'))
 app.use("/reports", require('./routes/district/reports.route'))
 app.use("/permission", require('./routes/district/permission.route'))
-
-
-
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on ${port}`)
