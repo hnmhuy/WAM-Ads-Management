ff572af3339ee421d546fd5fdf4b5c70a5bac933
nkhy-dev-3112 <nkhy.dev@gmail.com>
2023-11-17 12:51:29 +0700

    Modify row v2

--- controllers/district/reports.controller.js
+++ controllers/district/reports.controller.js
@@ -3,7 +3,7 @@ const { reports } = require('../../data')
 
 controller.show = (req, res) => {
     var name = "Danh sách báo cáo"
-    res.render('district/reports', { layout: 'list', name: name, heading: reports.heading, row: reports.row });
+    res.render('district/reports', { layout: 'list', name: name, heading: reports.heading, collapsible_row: reports.row });
 }
 
 module.exports = controller
