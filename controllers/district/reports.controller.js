69bef28d17e370d99044d581287bdfa31545a2f1
NhatPhi1804 <kenshiro.pn@gmail.com>
2023-11-19 19:10:31 +0700

    adjust ui

--- controllers/district/reports.controller.js
+++ controllers/district/reports.controller.js
@@ -2,8 +2,10 @@ const controller = {}
 const { reports } = require('../../data')
 
 controller.show = (req, res) => {
+    let table_data = require("../../reports_table_collapsible.json");
+    console.log(table_data);
     var name = "Danh sách báo cáo"
-    res.render('district/reports', { layout: 'list', name: name, heading: reports.heading, collapsible_row: reports.row });
+    res.render('district/reports', { layout: 'list', name: name, heading: reports.heading, collapsible_row: reports.row, table_data: table_data });
 }
 
 module.exports = controller
