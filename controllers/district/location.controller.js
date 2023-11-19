eb9376ab18633bc63c0416aea794ea80122744d0
NhatPhi1804 <kenshiro.pn@gmail.com>
2023-11-19 20:38:16 +0700

    adjust table

--- controllers/district/location.controller.js
+++ controllers/district/location.controller.js
@@ -3,7 +3,9 @@ const { locations } = require('../../data')
 
 controller.show = (req, res) => {
     var name = "Danh sách địa điểm quảng cáo"
-    res.render('district/location', { layout: 'list', name: name, heading: locations.heading, row: locations.row });
+    let data_table = require('../../ads_panel.json')
+    let ads_location_data = require('../../locations_data_collapsible.json')
+    res.render('district/location', { layout: 'list', name: name, table1: ads_location_data,table2:data_table });
 }
 
 module.exports = controller
