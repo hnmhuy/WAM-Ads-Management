f0c6b9664df9ae212758e44ab62e0f39ff999b45
bonheurisme <100709185+callmejippo@users.noreply.github.com>
2023-11-19 15:51:19 +0700

    define create-account-form in department_deligate

--- app.js
+++ app.js
@@ -1,114 +1,135 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const port = 3000 | process.env.port
-const expressHbs = require("express-handlebars")
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000 | process.env.port;
+const expressHbs = require("express-handlebars");
 app.use("/public", express.static(path.join(__dirname, "public")));
 const hbs = expressHbs.create({});
 
-app.engine("hbs", expressHbs.engine({
+app.engine(
+  "hbs",
+  expressHbs.engine({
     layoutsDir: __dirname + "/views/layouts",
     partialsDir: __dirname + "/views/partials",
     extname: "hbs",
-    defaultLayout: "layout"
-}))
-app.set("view engine", "hbs")
+    defaultLayout: "layout",
+  })
+);
+app.set("view engine", "hbs");
 
 // Register the helper
-hbs.handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+hbs.handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
 
 // Login
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 // Use routes of district
-app.use("/home", require('./routes/district/home.route'))
-app.use("/location", require('./routes/district/location.route'))
-app.use("/reports", require('./routes/district/reports.route'))
-app.use("/permission", require('./routes/district/permission.route'))
+app.use("/home", require("./routes/district/home.route"));
+app.use("/location", require("./routes/district/location.route"));
+app.use("/reports", require("./routes/district/reports.route"));
+app.use("/permission", require("./routes/district/permission.route"));
 
 app.get("/testing/dashboard", (req, res) => {
-    let navBarData = require("./nav_link.json");
-    //set all active = false
-    navBarData.nav_link.forEach((link) => {
-        link.active = false;
-    });
+  let navBarData = require("./nav_link.json");
+  //set all active = false
+  navBarData.nav_link.forEach((link) => {
+    link.active = false;
+  });
 
-    const target = navBarData.nav_link.find(
-        (link) => link.name === "dashboard"
-    );
-    if (target) {
-        target.active = true;
-    }
-    let componentDependcy = {
-        css: ["/public/css/department_dashboard.css"],
-        swipe: false,
-        title: "SỐ LIỆU THỐNG KÊ",
-    };
-    res.render("department/dashboard", {
-        layout: "department_layout",
-        css: componentDependcy.css,
-        swap: componentDependcy.swap,
-        title: componentDependcy.title,
-        nav_link: navBarData.nav_link,
-    });
+  const target = navBarData.nav_link.find((link) => link.name === "dashboard");
+  if (target) {
+    target.active = true;
+  }
+  let componentDependcy = {
+    css: ["/public/css/department_dashboard.css"],
+    swipe: false,
+    title: "SỐ LIỆU THỐNG KÊ",
+  };
+  res.render("department/dashboard", {
+    layout: "department_layout",
+    css: componentDependcy.css,
+    swap: componentDependcy.swap,
+    title: componentDependcy.title,
+    nav_link: navBarData.nav_link,
+  });
 });
 
 app.get("/testing/ads", (req, res) => {
-    let locations = require("./locations_data.json");
-    let navBarData = require("./nav_link.json");
-    navBarData.nav_link.forEach((link) => {
-        link.active = false;
-    });
+  let locations = require("./locations_data.json");
+  let navBarData = require("./nav_link.json");
+  navBarData.nav_link.forEach((link) => {
+    link.active = false;
+  });
 
-    const target = navBarData.nav_link.find((link) => link.name === "ads");
-    if (target) {
-        target.active = true;
-    }
-    let componentDependcy = {
-        css: ["/public/css/department_ads.css"],
-        swap: true,
-        title: "QUẢN LÝ QUẢNG CÁO",
-    };
-    res.render("department/ads_location", {
-        layout: "department_layout",
-        css: componentDependcy.css,
-        swap: componentDependcy.swap,
-        title: componentDependcy.title,
-        nav_link: navBarData.nav_link,
-        heading: locations.heading,
-        data_row: locations.data_row,
-    });
+  const target = navBarData.nav_link.find((link) => link.name === "ads");
+  if (target) {
+    target.active = true;
+  }
+  let componentDependcy = {
+    css: ["/public/css/department_ads.css"],
+    swap: true,
+    title: "QUẢN LÝ QUẢNG CÁO",
+  };
+  res.render("department/ads_location", {
+    layout: "department_layout",
+    css: componentDependcy.css,
+    swap: componentDependcy.swap,
+    title: componentDependcy.title,
+    nav_link: navBarData.nav_link,
+    heading: locations.heading,
+    data_row: locations.data_row,
+  });
 });
 
 app.get("/testing/label", (req, res) => {
   let navBarData = require("./nav_link.json");
   navBarData.nav_link.forEach((link) => {
-      link.active = false;
+    link.active = false;
   });
 
   const target = navBarData.nav_link.find((link) => link.name === "label");
   if (target) {
-      target.active = true;
+    target.active = true;
   }
   let componentDependcy = {
-      css: ["/public/css/department_label.css"],
-      swap: true,
-      title: "QUẢN LÝ DANH MỤC",
+    css: ["/public/css/department_label.css"],
+    swap: true,
+    title: "QUẢN LÝ DANH MỤC",
   };
   res.render("department/label", {
-      layout: "department_layout",
-      css: componentDependcy.css,
-      title: componentDependcy.title,
-      nav_link: navBarData.nav_link,
-
+    layout: "department_layout",
+    css: componentDependcy.css,
+    title: componentDependcy.title,
+    nav_link: navBarData.nav_link,
   });
 });
 
+app.get("/testing/deligate", (req, res) => {
+  let navBarData = require("./nav_link.json");
+  navBarData.nav_link.forEach((link) => {
+    link.active = false;
+  });
 
+  const target = navBarData.nav_link.find((link) => link.name === "deligate");
+  if (target) {
+    target.active = true;
+  }
+  let componentDependcy = {
+    css: ["/public/css/department_deligate.css"],
+    swap: true,
+    title: "QUẢN LÝ TÀI KHOẢN CÁN BỘ",
+  };
+  res.render("department/deligate", {
+    layout: "department_layout",
+    css: componentDependcy.css,
+    title: componentDependcy.title,
+    nav_link: navBarData.nav_link,
+  });
+});
 
 app.listen(port, (req, res) => {
-    console.log(`Server is running on ${port}`)
-})
+  console.log(`Server is running on ${port}`);
+});
