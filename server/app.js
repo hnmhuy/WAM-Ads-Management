dea148397245d78926ea1e398522c2893a1d8fb4
bonheurisme <100709185+callmejippo@users.noreply.github.com>
2023-11-26 01:05:05 +0700

    define label-ui-department

--- server/app.js
+++ server/app.js
@@ -85,15 +85,17 @@ app.get("/testing/label", (req, res) => {
         target.active = true;
     }
     let componentDependcy = {
-        css: ["/public/css/department_label.css"],
+        css: ["/public/css/department/label.css"],
         swap: true,
         title: "QUẢN LÝ DANH MỤC",
+        js: ["/public/js/department/label_ui_controllers.js"]
     };
     res.render("department/label", {
         layout: "department_layout",
         css: componentDependcy.css,
         title: componentDependcy.title,
         nav_link: navBarData.nav_link,
+        js: componentDependcy.js,
     });
 });
 
