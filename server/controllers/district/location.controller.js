2017d758352ad37fa00330edd6c394b5a9de6af3
nkhy-dev-3112 <nkhy.dev@gmail.com>
2023-12-06 09:46:00 +0700

    Fix lookup

--- server/controllers/district/location.controller.js
+++ server/controllers/district/location.controller.js
@@ -1,7 +1,9 @@
 const controller = {}
+const location = require('../../testing_vew_data/location.json');
 
 controller.show = (req, res) => {
     res.locals.page_name = "Danh sách điểm quảng cáo"
+    res.locals.data_rows = location.data_rows;
     res.render('district/location', { layout: 'district_layout' });
 }
 
