5fdac902a3c15930f08cf82b5c273e2f69e11759
Huy Huỳnh Nguyễn Minh <dev.hnmhuy@gmail.com>
2024-01-13 17:04:24 +0700

    update config for citizens

--- client-citizens/config.js
+++ client-citizens/config.js
@@ -1,4 +1,5 @@
 const config = {
     serverHost: "http://localhost:4000",
     mapboxToken: "pk.eyJ1IjoiZGV2LWhubWh1eSIsImEiOiJjbHBwYXY3ZW8weTdvMnBxbm85cnV2ZTFvIn0.6e8fwVmpoLxVSkyBWksYBg",
+    recaptchaKey: "6Lf3MDopAAAAAHflU59FHbEpSejyjGXIvb1rAgnq"
 }
